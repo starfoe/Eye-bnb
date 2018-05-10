6b66fad0ab4afa73a526f669707b91a80c33bad4
Ubuntu <ubuntu@ip-172-31-23-121.us-west-2.compute.internal>
2018-05-10 23:50:53 +0000

    Extracted gist features from CVPR indoor dataset

--- Notebook/image_process_tools.py
+++ Notebook/image_process_tools.py
@@ -2,13 +2,23 @@ from PIL import Image
 import pylab as pl
 import os
 import math
+from boto3 import client
+import boto3
+import pandas as pd
+import numpy as np
+import requests as req
+from io import BytesIO
+import gist
 
-def image_show(datapath,nums = 6,startImg = 0, **kwargs):
+
+
+def image_show(datapath,nums = 6,startImg = 0, local = True, **kwargs):
     '''
         Function: show nums of images involved in datapath folder starting from start_image or speicified by "imagelist" argument 
         Datapath: the folder containing images that to be shown
         nums: number of images that are gonna be displayed
         start_img: the index of the first picture
+        local: whether the image is an online image or saved locally(default). 
     '''
     fig_column = 4
     
@@ -23,17 +33,28 @@ def image_show(datapath,nums = 6,startImg = 0, **kwargs):
     
     for i in range(nums):         
         pl.subplot(math.ceil(nums/fig_column),fig_column,i+1,title = image_list[startImg+i])
-        img = Image.open(datapath+image_list[startImg+i])        
-        pl.axis('off')        
-        pl.imshow(img)
+        print(datapath+image_list[startImg+i])
+        if local:
+            img = Image.open(datapath+image_list[startImg+i])        
+            pl.axis('off')        
+            pl.imshow(img)
+        else:
+            response = req.get(datapath+image_list[startImg+i])
+            img = Image.open(BytesIO(response.content))
+            pl.axis('off')        
+            pl.imshow(img)
+       
+       
 
-def gist_extraction(imgFolder,nums = 9,startImg = 0, **kwargs):
+def gist_extraction(imgFolder,nums = 9,startImg = 0,local = False, **kwargs):
     '''
         Function: extract gist features for all the images in *imgFolder* or for *nums* of images starting from *startImg*
         Input:
             imgFolder: <string> file path of the image folder
             nums: <int> number of images that are gonna be displayed
             startImg: <int> the index of the first picture
+            local:<bool> whether images are online or saved locally
+            imageList:<list> a specific list of images
         Output: 
             featureX: <matrix> a matrix of size (nums,960) where each row represents an image feature
             nameList:<list> a list of image name in accordance to the feature matrix
@@ -48,17 +69,30 @@ def gist_extraction(imgFolder,nums = 9,startImg = 0, **kwargs):
     else:
         image_list = os.listdir(datapath)
         nums = min(nums,len(image_list)-startImg)
-        
-    for i in range(startImg,startImg+nums):
-        image_name = imgFolder+ image_list[i]
-        im = np.array(Image.open(image_name))
-        feature_for_i = gist.extract(im)
-        featureX = np.concatenate((featureX,feature_for_i.reshape(1,-1)),axis = 0)
-        nameList.append(image_name)
+    print("Processing {} images".format(nums))
+    
+    if local:    
+        for i in range(startImg,startImg+nums):
+            print("image {} is in processing".format(i))
+            image_name = imgFolder+ image_list[i]
+            im = np.array(Image.open(image_name))
+            feature_for_i = gist.extract(im)
+            featureX = np.concatenate((featureX,feature_for_i.reshape(1,-1)),axis = 0)
+            nameList.append(image_name)
+    else:
+        for i in range(startImg,startImg+nums):
+            print("image {} is in processing".format(i))
+            image_name = imgFolder + image_list[i]
+            print(image_name)
+            response = req.get(image_name)
+            im = np.array(Image.open(BytesIO(response.content)))
+            feature_for_i = gist.extract(im)
+            featureX = np.concatenate((featureX,feature_for_i.reshape(1,-1)),axis = 0)
+            nameList.append(image_name)           
     
     return featureX[1:,:], nameList       
 
-def show_similar_images(X,threshold,imgList,limit = 30,reverse = False):
+def show_similar_images(X,threshold,imgList,limit = 30,local = True, reverse = False):
     '''
         Function: show pairs of images between which the similarity is greater or less than threshold
         Input:
@@ -87,16 +121,29 @@ def show_similar_images(X,threshold,imgList,limit = 30,reverse = False):
         
         pl.subplot(nums_for_display,2,i*2+1,title = imgList[index_small[i,0]]+'\nSim:'+ \
                    str(round(X[index_small[i,0],index_small[i,1]],2)))
-        
-        img = Image.open(imgList[index_small[i,0]])        
-        pl.axis('off')        
-        pl.imshow(img)
-        
-        pl.subplot(nums_for_display,2,i*2+2,title = imgList[index_small[i,1]]+'\nloc:'+\
-                  str(index_small[i,:]))
-        img = Image.open(imgList[index_small[i,1]])   
-        pl.axis('off')
-        pl.imshow(img)
+        if local:
+            img = Image.open(imgList[index_small[i,0]])        
+            pl.axis('off')        
+            pl.imshow(img)
+
+            pl.subplot(nums_for_display,2,i*2+2,title = imgList[index_small[i,1]]+'\nloc:'+\
+                      str(index_small[i,:]))
+            img = Image.open(imgList[index_small[i,1]])   
+            pl.axis('off')
+            pl.imshow(img)
+        else:
+            response = req.get(imgList[index_small[i,0]])
+            img = Image.open(BytesIO(response.content))
+            pl.axis('off')        
+            pl.imshow(img)
+            
+            pl.subplot(nums_for_display,2,i*2+2,title = imgList[index_small[i,1]]+'\nloc:'+\
+                      str(index_small[i,:]))
+            response = req.get(imgList[index_small[i,1]])
+            img = Image.open(BytesIO(response.content)) 
+            pl.axis('off')
+            pl.imshow(img)
+            
 
         
 def s3_list_files(bucket_name,prefix,layers):
@@ -127,3 +174,28 @@ def s3_list_files(bucket_name,prefix,layers):
             dict_parse[x].append(string[-1*x])    
 
     return pd.DataFrame(dict_parse)
+
+def feature_df_assembly(X,imgNameList,write_to_file = False, **kwargs):
+    '''
+        Function: assemble features and meta info into a dataframe and write to .csv file\
+        Input:
+            X:<array> feature matrix
+            imgNameList:<list> name of the original images
+            write_to_file:<bool> if true, the dataframe will be save to a csv file in the current work directory
+            class_labels:<list> if known
+        Output:
+            dataframe
+    '''
+    if len(X) != len(imgNameList):
+        raise Exception('imgNameList should have the same length as features')
+    
+    dict_tmp = {'features':X.tolist(),'img_names':imgNameList}
+    df_return = pd.DataFrame(dict_tmp)
+    
+    if 'labels' in kwargs:
+        labels = kwargs['labels']
+        df_return['class_label'] = labels
+    
+    if write_to_file:
+        df_return.to_csv(write_to_file)
+    return df_return
