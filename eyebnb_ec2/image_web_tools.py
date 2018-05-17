3bd1ff2cc1174aa2c4817cc9c9bba8c976bcc116
starfoe <ccchichen@hotmail.com>
2018-05-17 04:55:08 +0000

    Completed feature extraction and similarity return function

--- eyebnb_ec2/image_web_tools.py
+++ eyebnb_ec2/image_web_tools.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import pylab as pl
+# import pylab as pl
 import os
 import math
 from boto3 import client
@@ -9,8 +9,10 @@ import numpy as np
 import requests as req
 from io import BytesIO
 import gist
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_distances
 
-def feature_extraction(feature_name = 'gist', img_array):
+def feature_extraction(img_array,feature_name = 'gist'):
     '''
     Function: extraction multiple features from an image
     Input:
@@ -18,21 +20,56 @@ def feature_extraction(feature_name = 'gist', img_array):
         img_array: <array> an array converted image
     Output: array or array-like feature
     '''
-    if feature_name = 'gist':
-        return gist.extract(im)
+    if feature_name == 'gist':
+        return gist.extract(img_array)
     else:
         return [] #to be extended
 
-def feature_distance(metric ='cosine',feature1,feature2):
+def find_closest_img(featureX,features_matrix,mask = {}, n_selected = 20):
     '''
-    Function:calculate feature distance of feature1 and feature 2
+    Function: find top *n_selected* of apts based on image similarities
     Input:
-        feature1,feature2: <vector-like> two features
-        metric: <string> 'cosine','cor','euclidean' 
+        featureX: <array> an image feature
+        feature_matrix: database image features
+        n_selected: number of similar images
+        mask: <set> customized image indice that need to be filtered out 
     Output:
-        dis:<float> the distance of the two features based on the metric
+        index:<int> index of images that are selected
+    '''
     
+    distance_vector = cosine_similarity(featureX.reshape(1,-1),features_matrix)#can change to other similarity measurements
+    percentile = [99,95,90,85,80,75,70,1]
+    for perc in percentile:
+        threshold = np.percentile(distance_vector,perc)
+        # times 10 to make sure that there would be enough candidate images
+        if len(np.argwhere(distance_vector > threshold)) > 10* n_selected:            
+            break            
+    index_filtered = np.argwhere(distance_vector > threshold)
+    print('mask type is {}'.format(type(mask)))
+#     current_mask_len = len(mask)
     
-    
+    rounds = 10
+    while(rounds > 0):
+        candidate_small = distance_vector[index_filtered[:,0],index_filtered[:,1]]
+        top_similar = np.argsort(candidate_small)[::-1][0:n_selected+len(mask)]
+        current_selected_index = [x for x in index_filtered[top_similar][:,1] \
+                                  if x not in mask]
+        assert(len(current_selected_index) == n_selected)
+        current_feature_matrix = features_matrix[current_selected_index]
+        validation_dist = cosine_similarity(current_feature_matrix)
+
+        duplicated_index_tmp = np.argwhere(validation_dist > 0.9999)
+        duplicated_index = duplicated_index_tmp[np.argwhere(duplicated_index_tmp[:,0]\
+                                                    <duplicated_index_tmp[:,1])[:,0]][:,1]
+        if len(duplicated_index) == 0:
+            break
+            
+        if len(mask) == 0:
+            mask = set(np.array(current_selected_index)[duplicated_index])
+        else:
+            mask.update(np.array(current_selected_index)[duplicated_index])
+        rounds -= 1
+        
+    return current_selected_index,mask 
 
     
--- eyebnb_ec2/web_query.py
+++ eyebnb_ec2/web_query.py
@@ -11,6 +11,7 @@ import boto3
 from PIL import Image
 from io import BytesIO
 from sklearn.metrics.pairwise import cosine_similarity
+from eyebnb_ec2.image_web_tools import *
 
 def load_features(path):
     df_file = pd.read_pickle(path)
@@ -19,64 +20,21 @@ def load_features(path):
     feature_matrix = df_file[list(range(960))].values
     return feature_matrix,img_file_path,apt_name
     
-
-def find_closest_img(featureX,features_matrix,mask = {}, n_selected = 20):
-    '''
-    Function: find top *n_selected* of apts based on image similarities
-    Input:
-        featureX: <array> an image feature
-        feature_matrix: database image features
-        n_selected: number of similar images
-        mask: <set> customized image indice that need to be filtered out 
-    Output:
-        index:<int> index of images that are selected
-    '''
-   
-    distance_vector = cosine_similarity(featureX,features_matrix)#can change to other similarity measurements
-    percentile = [99,95,90,85,80,75,70,1]
-    for perc in percentile:
-        threshold = np.percentile(distance_vector,perc)
-        # times 10 to make sure that there would be enough candidate images
-        if len(np.argwhere(distance_vector > threshold)) > 10* n_selected:            
-            break            
-    index_filtered = np.argwhere(distance_vector > threshold)
-    current_mask_len = len(mask)
-    rounds = 10
-    while(rounds > 0):
-        candidate_small = distance_vector[index_filtered[:,0],index_filtered[:,1]]
-        top_similar = np.argsort(candidate_small)[::-1][0:n_selected+len(mask)]
-        current_selected_index = [x for x in index_filtered[top_similar][:,1] \
-                                  if x not in mask]
-        assert(len(current_selected_index) == n_selected)
-        current_feature_matrix = features_matrix[current_selected_index]
-        validation_dist = cosine_similarity(current_feature_matrix)
-
-        duplicated_index_tmp = np.argwhere(validation_dist > 0.9999)
-        duplicated_index = duplicated_index_tmp[np.argwhere(duplicated_index_tmp[:,0]\
-                                                    <duplicated_index_tmp[:,1])[:,0]][:,1]
-        if len(duplicated_index) == 0:
-            break
-            
-        if len(mask) == 0:
-            mask = set(np.array(current_selected_index)[duplicated_index])
-        else:
-            mask.update(np.array(current_selected_index)[duplicated_index])
-        rounds -= 1
-        
-    return current_selected_index,mask 
-
-def web_query(url_input,return_top = 20,feature_path):
-    feature_matrix,img_file_path,apt_name = load_features(feature_path)
-    try:
-        response = req.get(url_input)
-        im = np.array(Image.open(BytesIO(response.content)))
-        feature_input = feature_extraction(feature_name = 'gist',im)
-    except:
-        break
+def web_query(url_input,feature_path,return_top = 20):
+    feature_matrix,img_file_path,apt_id = load_features(feature_path)
+#     try:
+    response = req.get(url_input)
+    #im = np.array(Image.open(BytesIO(response.content)))
+    im = np.asarray(Image.open(BytesIO(response.content)))
+#         print('image file is {}'.format(im))
+    feature_input = feature_extraction(im,feature_name = 'gist')
+    
+#     except:
+#         raise IOError('Cannot open the URL')
         #raise error
-    selected_index =  find_closest_img(feature_input,feature_matrix,return_top)[0]
+    selected_index =  find_closest_img(feature_input,feature_matrix,{},return_top)[0]
     selected_apt_id = apt_id[selected_index]
-    selected_image_path = filepath[selected_index]
+    selected_image_path = img_file_path[selected_index]
     ######Temporary searching from Json File#######
     bucket_name = "chen-gal-test"
     s3 = boto3.client("s3")
@@ -86,9 +44,9 @@ def web_query(url_input,return_top = 20,feature_path):
     fake_database= json.loads(response['Body'].read())
     #####Extract data from the fake database#######
     fake_result = {}
-    for i in selected_apt:
+    for i,idx in enumerate(selected_apt_id):
         tmp = {}
-        x = fake_database[str(i)]
+        x = fake_database[str(idx)]
         tmp['apt_name'] = x['apt_name']
         tmp['canonical_url'] = x['info']['canonical_url']
         tmp['room_type'] = x['info']['room_type']
@@ -96,12 +54,16 @@ def web_query(url_input,return_top = 20,feature_path):
         tmp['host_about'] = x['info']['host_about']
         tmp['overall_rating']=x['info']['overall_rating']
         tmp['review_highlight'] = x['info']['review_highlight']
+#         print('current file name is {}'.format(selected_image_path[i]))
+        tmp['which_one'] = list(selected_image_path)[i]
         fake_result[str(i)]=tmp
+    
+    result = json.dumps(fake_result)
 
     ######
     #if there are duplicated aparts in the recommending results, then keep only one of them    
     ######
-    return selected_apt_id,selected_image_path
+    return result
     
     
     
