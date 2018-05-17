750af29cb1da5458186e6411d466606d03731195
starfoe <ccchichen@hotmail.com>
2018-05-17 23:50:31 +0000

    Added table to html

--- eyebnb_ec2/web_query.py
+++ eyebnb_ec2/web_query.py
@@ -25,8 +25,10 @@ def web_query(url_input,feature_path,return_top = 20):
 #     try:
     response = req.get(url_input)
     #im = np.array(Image.open(BytesIO(response.content)))
-    im = np.asarray(Image.open(BytesIO(response.content)))
+    img_tmp = Image.open(BytesIO(response.content))
+    im = np.asarray(img_tmp.resize((600,400)))
 #         print('image file is {}'.format(im))
+    
     feature_input = feature_extraction(im,feature_name = 'gist')
     
 #     except:
@@ -43,27 +45,27 @@ def web_query(url_input,feature_path,return_top = 20):
                              Key=database_json_short)
     fake_database= json.loads(response['Body'].read())
     #####Extract data from the fake database#######
-    fake_result = {}
+    
     for i,idx in enumerate(selected_apt_id):
         tmp = {}
         x = fake_database[str(idx)]
         tmp['apt_name'] = x['apt_name']
         tmp['canonical_url'] = x['info']['canonical_url']
         tmp['room_type'] = x['info']['room_type']
-        tmp['room_compacity'] = x['info']['room_capacity']
+        tmp['room_capacity'] = x['info']['room_capacity']
         tmp['host_about'] = x['info']['host_about']
         tmp['overall_rating']=x['info']['overall_rating']
-        tmp['review_highlight'] = x['info']['review_highlight']
+        tmp['room_type'] = x['info']['room_type']
 #         print('current file name is {}'.format(selected_image_path[i]))
         tmp['which_one'] = list(selected_image_path)[i]
-        fake_result[str(i)]=tmp
+        yield tmp
     
-    result = json.dumps(fake_result)
+#     result = json.dumps(fake_result)
 
     ######
     #if there are duplicated aparts in the recommending results, then keep only one of them    
     ######
-    return result
+#     return result
     
     
     
